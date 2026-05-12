8b9b8af9549223c15a36dd772d14054f500d75da
Nebiyu Talefe <nebiyuelias1@gmail.com>
2026-05-12 16:58:01 +0800

    chore: allow name updates

--- backend/app/controllers/users/registrations_controller.rb
+++ backend/app/controllers/users/registrations_controller.rb
@@ -6,6 +6,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :avatar_url)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :avatar_url)
   end
 end
