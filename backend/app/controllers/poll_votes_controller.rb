bbb4f14ab0b4c8c945b5287d234c55248feea34c
Nebiyu Talefe <nebiyuelias1@gmail.com>
2026-05-19 23:07:03 +0800

    feat: allow voting

---
+++ backend/app/controllers/poll_votes_controller.rb
@@ -0,0 +1,55 @@
+class PollVotesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_book_club
+  before_action :set_book_read
+  before_action :set_poll
+
+  def create
+    poll_option_ids = Array(params[:poll_option_ids]).reject(&:blank?)
+    @poll_options = @poll.poll_options.includes(:poll_votes)
+
+    if poll_option_ids.empty?
+      @error_message = "Select at least one option to vote."
+      return respond_with_error
+    end
+
+    options = @poll_options.select { |option| poll_option_ids.include?(option.id.to_s) }
+
+    begin
+      PollVote.transaction do
+        options.each do |option|
+          PollVote.create!(poll_option: option, user: current_user)
+        end
+      end
+    rescue ActiveRecord::RecordInvalid => e
+      @error_message = e.record.errors.full_messages.to_sentence
+      return respond_with_error
+    end
+
+    respond_to do |format|
+      format.html { redirect_to book_club_book_read_path(@book_club, @book_read), notice: "Thanks for voting." }
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_book_club
+    @book_club = BookClub.find(params[:book_club_id])
+  end
+
+  def set_book_read
+    @book_read = @book_club.book_reads.find(params[:book_read_id])
+  end
+
+  def set_poll
+    @poll = @book_read.poll
+  end
+
+  def respond_with_error
+    respond_to do |format|
+      format.html { redirect_to book_club_book_read_path(@book_club, @book_read), alert: @error_message }
+      format.turbo_stream { render :create, status: :unprocessable_entity }
+    end
+  end
+end
--- backend/app/javascript/controllers/poll_vote_form_controller.js
+++ backend/app/javascript/controllers/poll_vote_form_controller.js
@@ -11,6 +11,13 @@ export default class extends Controller {
     this.updateSubmitState()
   }
 
+  reset() {
+    this.optionTargets.forEach((option) => {
+      option.checked = false
+    })
+    this.updateSubmitState()
+  }
+
   updateSubmitState() {
     const hasSelection = this.optionTargets.some((option) => option.checked)
     this.submitTarget.disabled = !hasSelection
