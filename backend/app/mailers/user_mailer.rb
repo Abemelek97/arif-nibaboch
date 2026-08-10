33763e13e1524f27d37c299c4c55331e2b3d2a94
Nebiyu Talefe <nebiyuelias1@gmail.com>
2026-08-10 22:16:45 +0800

    chore: add book read update job and email

--- backend/app/mailers/user_mailer.rb
+++ backend/app/mailers/user_mailer.rb
@@ -10,7 +10,10 @@ class UserMailer < ApplicationMailer
     @time_zone = params[:time_zone] || "Taipei"
 
     cal = Icalendar::Calendar.new
+    cal.ip_method = "REQUEST"
     cal.event do |e|
+      e.uid         = calendar_event_uid
+      e.sequence    = @book_read.calendar_sequence || 0
       # Explicitly marking as UTC ensures calendars auto-adjust to user's local time
       e.dtstart     = Icalendar::Values::DateTime.new(@book_read.meetup_time.utc, tzid: "UTC")
       e.dtend       = Icalendar::Values::DateTime.new((@book_read.meetup_time + 2.hours).utc, tzid: "UTC")
@@ -18,13 +21,50 @@ class UserMailer < ApplicationMailer
       e.description = "RSVP for #{@book_read.book_club.name}"
       e.location    = @book_read.meetup_location
       e.ip_class    = "PRIVATE"
+      e.status      = "CONFIRMED"
     end
 
     attachments["invite.ics"] = {
-      mime_type: "text/calendar",
+      mime_type: "text/calendar; method=REQUEST",
       content: cal.to_ical
     }
 
     mail(to: @user.email, subject: "RSVP Confirmation: #{@book_read.book&.title || 'Book Read'}")
+  end
+
+  def book_read_updated_invite
+    @rsvp = params[:rsvp]
+    @user = @rsvp.user
+    @book_read = @rsvp.book_read
+
+    @time_zone = params[:time_zone] || "Taipei"
+
+    cal = Icalendar::Calendar.new
+    cal.ip_method = "REQUEST"
+    cal.event do |e|
+      e.uid         = calendar_event_uid
+      e.sequence    = @book_read.calendar_sequence || 0
+      e.dtstart     = Icalendar::Values::DateTime.new(@book_read.meetup_time.utc, tzid: "UTC")
+      e.dtend       = Icalendar::Values::DateTime.new((@book_read.meetup_time + 2.hours).utc, tzid: "UTC")
+      e.summary     = "Updated: Book Read: #{@book_read.book&.title || 'Discussion'}"
+      e.description = "RSVP for #{@book_read.book_club.name} (Schedule Updated)"
+      e.location    = @book_read.meetup_location
+      e.ip_class    = "PRIVATE"
+      e.status      = "CONFIRMED"
+    end
+
+    attachments["invite.ics"] = {
+      mime_type: "text/calendar; method=REQUEST",
+      content: cal.to_ical
+    }
+
+    mail(to: @user.email, subject: "[Updated] Book Read: #{@book_read.book&.title || 'Book Read'}")
+  end
+
+  private
+
+  def calendar_event_uid
+    mail_domain = Rails.configuration.x.mail_from_domain
+    "book-read-#{@book_read.id}-rsvp-#{@rsvp.id}@#{mail_domain}"
   end
 end
