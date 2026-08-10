74858a63c5417141faadec67a0ad6fe2a20a8c70
Nebiyu Talefe <nebiyuelias1@gmail.com>
2026-08-10 22:21:34 +0800

    chore: avoid duplication of user_mailer.rb

--- backend/app/mailers/user_mailer.rb
+++ backend/app/mailers/user_mailer.rb
@@ -1,14 +1,28 @@
 class UserMailer < ApplicationMailer
   default from: "#{Rails.configuration.x.app_name} <noreply@#{Rails.configuration.x.mail_from_domain}>" # this domain must be verified with Resend
 
+  before_action :set_rsvp_params, only: [ :rsvp_confirmation, :book_read_updated_invite ]
+
   def rsvp_confirmation
+    attach_calendar_invite!
+    mail(to: @user.email, subject: "RSVP Confirmation: #{@book_read.book&.title || 'Book Read'}")
+  end
+
+  def book_read_updated_invite
+    attach_calendar_invite!(summary_prefix: "Updated: ", description_suffix: " (Schedule Updated)")
+    mail(to: @user.email, subject: "[Updated Calendar Invite] Book Read: #{@book_read.book&.title || 'Book Read'}")
+  end
+
+  private
+
+  def set_rsvp_params
     @rsvp = params[:rsvp]
     @user = @rsvp.user
     @book_read = @rsvp.book_read
-
-    # Use the passed time_zone or default to Taipei (UTC+8)
     @time_zone = params[:time_zone] || "Taipei"
+  end
 
+  def attach_calendar_invite!(summary_prefix: nil, description_suffix: nil)
     cal = Icalendar::Calendar.new
     cal.ip_method = "REQUEST"
     cal.event do |e|
@@ -17,8 +31,8 @@ class UserMailer < ApplicationMailer
       # Explicitly marking as UTC ensures calendars auto-adjust to user's local time
       e.dtstart     = Icalendar::Values::DateTime.new(@book_read.meetup_time.utc, tzid: "UTC")
       e.dtend       = Icalendar::Values::DateTime.new((@book_read.meetup_time + 2.hours).utc, tzid: "UTC")
-      e.summary     = "Book Read: #{@book_read.book&.title || 'Discussion'}"
-      e.description = "RSVP for #{@book_read.book_club.name}"
+      e.summary     = "#{summary_prefix}Book Read: #{@book_read.book&.title || 'Discussion'}"
+      e.description = "RSVP for #{@book_read.book_club.name}#{description_suffix}"
       e.location    = @book_read.meetup_location
       e.ip_class    = "PRIVATE"
       e.status      = "CONFIRMED"
@@ -28,40 +42,7 @@ class UserMailer < ApplicationMailer
       mime_type: "text/calendar; method=REQUEST",
       content: cal.to_ical
     }
-
-    mail(to: @user.email, subject: "RSVP Confirmation: #{@book_read.book&.title || 'Book Read'}")
   end
-
-  def book_read_updated_invite
-    @rsvp = params[:rsvp]
-    @user = @rsvp.user
-    @book_read = @rsvp.book_read
-
-    @time_zone = params[:time_zone] || "Taipei"
-
-    cal = Icalendar::Calendar.new
-    cal.ip_method = "REQUEST"
-    cal.event do |e|
-      e.uid         = calendar_event_uid
-      e.sequence    = @book_read.calendar_sequence || 0
-      e.dtstart     = Icalendar::Values::DateTime.new(@book_read.meetup_time.utc, tzid: "UTC")
-      e.dtend       = Icalendar::Values::DateTime.new((@book_read.meetup_time + 2.hours).utc, tzid: "UTC")
-      e.summary     = "Updated: Book Read: #{@book_read.book&.title || 'Discussion'}"
-      e.description = "RSVP for #{@book_read.book_club.name} (Schedule Updated)"
-      e.location    = @book_read.meetup_location
-      e.ip_class    = "PRIVATE"
-      e.status      = "CONFIRMED"
-    end
-
-    attachments["invite.ics"] = {
-      mime_type: "text/calendar; method=REQUEST",
-      content: cal.to_ical
-    }
-
-    mail(to: @user.email, subject: "[Updated] Book Read: #{@book_read.book&.title || 'Book Read'}")
-  end
-
-  private
 
   def calendar_event_uid
     mail_domain = Rails.configuration.x.mail_from_domain
