c603c6c5f914ffa557da474c0ca6bf56aefba5ea
Nebiyu Talefe <nebiyuelias1@gmail.com>
2026-08-22 22:28:03 +0800

    feat: promote rsvp waitlist when someone cancels

--- backend/test/models/book_read_rsvp_test.rb
+++ backend/test/models/book_read_rsvp_test.rb
@@ -68,4 +68,55 @@ class BookReadRsvpTest < ActiveSupport::TestCase
       rsvp.update!(status: :cancelled)
     end
   end
+
+  test "auto-promotes oldest waitlisted user when a going user cancels" do
+    @book_read.update!(max_capacity: 2)
+    BookReadRsvp.create!(book_read: @book_read, user: users(:one), status: :going)
+    going2 = BookReadRsvp.create!(book_read: @book_read, user: users(:two), status: :going)
+    waitlisted = BookReadRsvp.create!(book_read: @book_read, user: users(:three), status: :waitlisted)
+
+    assert_enqueued_emails 1 do
+      BookReadRsvp.rsvp!(book_read: @book_read, user: going2.user, status: :cancelled)
+    end
+
+    assert_equal "going", waitlisted.reload.status
+  end
+
+  test "does not promote when no waitlisted users exist" do
+    @book_read.update!(max_capacity: 2)
+    BookReadRsvp.create!(book_read: @book_read, user: users(:one), status: :going)
+    going2 = BookReadRsvp.create!(book_read: @book_read, user: users(:two), status: :going)
+
+    assert_enqueued_emails 0 do
+      BookReadRsvp.rsvp!(book_read: @book_read, user: going2.user, status: :cancelled)
+    end
+
+    assert_equal "cancelled", going2.reload.status
+  end
+
+  test "does not promote when max_capacity is nil" do
+    # @book_read max_capcity is nil
+    going = BookReadRsvp.create!(book_read: @book_read, user: users(:one), status: :going)
+    waitlisted = BookReadRsvp.create!(book_read: @book_read, user: users(:two), status: :waitlisted)
+
+    assert_enqueued_emails 0 do
+      BookReadRsvp.rsvp!(book_read: @book_read, user: going.user, status: :cancelled)
+    end
+
+    assert_equal "waitlisted", waitlisted.reload.status
+  end
+
+  test "promotes waitlisted users in FIFO order" do
+    @book_read.update!(max_capacity: 2)
+    going1 = BookReadRsvp.create!(book_read: @book_read, user: users(:one), status: :going)
+    BookReadRsvp.create!(book_read: @book_read, user: users(:two), status: :going)
+    first_wait = BookReadRsvp.create!(book_read: @book_read, user: users(:three), status: :waitlisted)
+    fourth_user = User.create!(email: "user4@example.com", password: "password123", username: "testuser4", name: "Test User Four", confirmed_at: Time.current)
+    second_wait = BookReadRsvp.create!(book_read: @book_read, user: fourth_user, status: :waitlisted)
+
+    BookReadRsvp.rsvp!(book_read: @book_read, user: going1.user, status: :cancelled)
+
+    assert_equal "going", first_wait.reload.status
+    assert_equal "waitlisted", second_wait.reload.status
+  end
 end
